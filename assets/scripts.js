d12853994c21f14de7a06e6703c65edbf0c18306
Jessica Marshall <jessica.marshall14@gmail.com>
2018-03-03 23:22:44 -0500

    chicken on a raft

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,13 +1,17 @@
+let displayText = 'press a new key for a cool link from me'
+
 document.onkeypress = function (e) {
+  // console.log(constants)
   move()
   changeColor()
-  // console.log('window width', window.width)
+  postLink()
 }
 
 function move () {
+  document.getElementById('text').innerHTML = displayText
   let elem = document.getElementById('text')
-  console.log(elem.offsetWidth)
-  console.log(elem.offsetHeight)
+  // console.log(elem.offsetWidth)
+  // console.log(elem.offsetHeight)
   elem.style.left = generateLocation(0, elem.offsetWidth)
   elem.style.top = generateLocation(1, elem.offsetHeight)
 }
@@ -21,9 +25,18 @@ function generateLocation (i, elemConst) {
   let windowConst = [window.innerWidth, window.innerHeight]
   let randNum = Math.floor(Math.random() * 100)
   let locPerc = randNum * (windowConst[i] - elemConst) / windowConst[i]
+  // console.log(i, locPerc)
   return locPerc.toString() + '%'
 }
 
 function generateColor () {
   return '#' + (Math.random() * 0xFFFFFF << 0).toString(16)
+}
+
+function postLink () {
+  let a = document.createElement('a')
+  let linkText = document.createTextNode('_click me_')
+  a.appendChild(linkText)
+  a.href = 'https://chickenonaraft.com/'
+  document.body.appendChild(a)
 }
